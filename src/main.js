ce6710526cfb23c8034328304c2a8dae51d18cc7
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-09-22 10:05:31 +0200

    init

---
+++ src/main.js
@@ -0,0 +1,32 @@
+import './assets/main.scss'
+
+import {createApp} from 'vue'
+import {createRouter, createWebHashHistory} from "vue-router";
+import RegisterView from "@/views/RegisterView.vue";
+import ScannerView from "@/views/ScannerView.vue";
+import App from "@/App.vue";
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: [
+        {
+            path: "/register",
+            name: "register",
+            component: RegisterView,
+        },
+        {
+            path: "/scanner",
+            name: "scanner",
+            component: ScannerView
+        },
+        {
+            path: "/",
+            name: "base",
+            redirect: "/register",
+        },
+    ]
+})
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app')
---
+++ src/views/ScannerView.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <div>
+        Scanner
+    </div>
+</template>
+
+<style scoped>
+
+</style>
