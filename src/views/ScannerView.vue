3c28b579befed2e0ed5c8ab473c32df054f5ea37
Jonas Pfeifer <jonaspfeifer@drei.at>
2023-11-04 22:21:38 +0100

    removed notification

--- src/views/ScannerView.vue
+++ src/views/ScannerView.vue
@@ -30,8 +30,6 @@ function onDetect(detectedCodes: { rawValue: string }[]) {
 
     const [type, qrValue] = value.split("/");
 
-    toastStore.addNotification("info",type);
-
     if (type === "redeem") {
         axios.get(backendIP + `scan/canRedeem/${userID}&${qrValue}`).then(() => {
             axios.get(backendIP + `scan/redeem/${userID}&${qrValue}`).then(response => {
